4870d5b940d68c4c875ce3d95073a65f3b45404d
Miguel Ceriani <miguel.ceriani@gmail.com>
2026-07-26 16:02:01 +0200

    Fix bug in template decomposition

--- compileView.js
+++ compileView.js
@@ -86,13 +86,22 @@ function nonEmptySubsets(items) {
     return subsets(items).slice(1);
 }
 
+function replaceTemplate(query, newTemplate) {
+    return visitQuery(query, {
+        postVisitQuery: pattern => {
+            pattern.template = newTemplate
+            return pattern;
+        }
+    });
+}
+
 function decomposeConstruct(construct) {
     // Split a CONSTRUCT with multiple template triples into a list of
     // single-triple CONSTRUCTs. For each template triple, replace the free
     // variable role names with the canonical `DEFAULT_VARS` so subsequent
     // comparisons and merging operate on a normalized form.
     return construct.template.map(templateTriple => replaceVars(
-        construct,
+        replaceTemplate(construct, [templateTriple]),
         Object.fromEntries(
             freeRoles(templateTriple).map(role => 
                 [templateTriple[role].value, DEFAULT_VARS[role]]
